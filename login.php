156439314d7c687ef2e3776b06863e9c393de20a
Jinn Ha <haphu155722@gmail.com>
2024-06-16 09:46:30 -0500

    logo update

--- login.php
+++ login.php
@@ -7,7 +7,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>stardustr - Log in</title>
+    <title>stardust - Log in</title>
 
     <!-- Include stylesheets -->
     <link rel="stylesheet" href="main.css">
